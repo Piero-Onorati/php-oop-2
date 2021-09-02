86c27abe21251e1e606877ee6d50cfb309f56ade
Piero <piero.onorati@virgilio.it>
2021-09-02 17:32:04 +0200

    add exeption

--- classes/Product.php
+++ classes/Product.php
@@ -15,7 +15,11 @@
         protected $slug;
 
         function __construct($_id, $_name, $_description, $_price, $_manufacturer, $_image, $_slug ){
-            $this->id = $_id;
+            if (is_null($_id)) {
+                $this->id = $this->genId();
+            }else{
+                throw new Exception("The id field must be null. It will be generated automatically");
+            }
             $this->name = $_name;
             $this->description = $_description;
             $this->price = $_price;
@@ -23,5 +27,20 @@
             $this->image = $_image;
             $this->slug = $_slug;
         }
+
+        public function genId(){
+            $prefix='a0';
+            $output= $prefix;
+            $chars = range('a', 'z');
+            $digits = range(0, 9);
+            $parts = array_merge($chars, $digits);
+            for ($i=0; $i < 8; $i++) { 
+                $output .=  $parts[mt_rand(0, count($parts) - 1)];
+            }
+            return $output;
+        }
+
+       
+        
 
     }
--- index.php
+++ index.php
@@ -4,14 +4,22 @@
     require_once __DIR__ . '/classes/Clothing.php';
     require_once __DIR__ . '/classes/Shoes.php';
 
-
     // Classe Genitore
-    $prodotto = new Product(3,'lorem','ipsum', 6,'dolor', 'sit', 'amet');
+    try {
+        $prodotto = new Product(null,'lorem','ipsum', 6,'dolor', 'sit', 'amet');
+    } catch (Exception $e) {
+        echo 'Errore: ' . $e->getMessage();
+    }
+    
     var_dump($prodotto);
 
     //Classe Figlia CLOTHING
 
-    $t_shirt = new Clothing(3,'Join Life','T-shirt in felpa blu in coordinato', 15.99,'Pull&Bear', 'https//', '...', 'S', 'maglietta');
+    try {
+        $t_shirt = new Clothing(null,'Join Life','T-shirt in felpa blu in coordinato', 15.99,'Pull&Bear', 'https//', '...', 'S', 'maglietta');
+    } catch (Exception $e) {
+        echo 'Errore: ' . $e->getMessage();
+    }
 
     $t_shirt->setInSale(true);
     $t_shirt->getInSale();
@@ -19,12 +27,15 @@
     var_dump($t_shirt);
 
     //Classe Figlia SHOES
-
-    $scarpa = new Shoes(3,'Nike Jordan MA2','Sneakers beige pietra', 128.99,'Nike', 'https//', '...', '40', 'sneakers');
+    try {
+        $scarpa = new Shoes(0,'Nike Jordan MA2','Sneakers beige pietra', 128.99,'Nike', 'https//', '...', '40', 'sneakers');
+    } catch (Exception $e) {
+        echo 'Errore: ' . $e->getMessage();
+    }
 
     $scarpa->setInSale(false);
     $scarpa->getInSale();
-    
+
     var_dump($scarpa);
 
     
