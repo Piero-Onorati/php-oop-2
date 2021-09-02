dcb6e7b78af2e78204634e3bdd5a295744d150a4
Piero <piero.onorati@virgilio.it>
2021-09-02 15:30:22 +0200

    create class Product

--- classes/Product.php
+++ classes/Product.php
@@ -10,13 +10,13 @@
         public $slug;
 
         function __construct($_id, $_name, $_description, $_price, $_manufacturer, $_image, $_slug ){
-            $this->$id = $_id;
-            $this->$name = $_name;
-            $this->$description = $_description;
-            $this->$price = $_price;
-            $this->$manufacturer = $_manufacturer;
-            $this->$image = $_image;
-            $this->$slug = $_slug;
+            $this->id = $_id;
+            $this->name = $_name;
+            $this->description = $_description;
+            $this->price = $_price;
+            $this->manufacturer = $_manufacturer;
+            $this->image = $_image;
+            $this->slug = $_slug;
         }
 
     }
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+
+    require_once __DIR__ . '/classes/Product.php';
+
+    // $borsa = new Product(3,'f','g', 6,'a', 'b', 'c');
+    // var_dump($borsa);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
