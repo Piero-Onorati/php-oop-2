c675995950b5946ab53408349f3f7e8f7ac48fd8
Piero <piero.onorati@virgilio.it>
2021-09-02 15:20:33 +0200

    init

---
+++ classes/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+    class Product {
+        public $id;
+        public $name;
+        public $description;
+        public $price;
+        public $manufacturer;
+        public $image;
+        public $slug;
+
+        function __construct($_id, $_name, $_description, $_price, $_manufacturer, $_image, $_slug ){
+            $this->$id = $_id;
+            $this->$name = $_name;
+            $this->$description = $_description;
+            $this->$price = $_price;
+            $this->$manufacturer = $_manufacturer;
+            $this->$image = $_image;
+            $this->$slug = $_slug;
+        }
+
+    }
